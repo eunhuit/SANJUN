989fe2869c8df165611cef61066073b81cc71605
eunhuit <51870454+ixxeh-p@users.noreply.github.com>
2024-09-10 12:04:29 +0900

    인터럽트
    interrupt 구성

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 2024-09-10 오전 11:32:53
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(INT0_vect) {
+	PORTA=0xFF;
+}
+
+ISR(INT1_vect) {
+	PORTA=0x00;
+}
+
+ int main(void)
+{
+	DDRA=0xff;
+	EICRA=0b00001011;
+	EIMSK=0b00000011;
+	sei();
+    while (1) 
+    {
+		
+		
+    }
+}
+
