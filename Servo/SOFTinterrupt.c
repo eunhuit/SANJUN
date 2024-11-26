5d50ebd0e1fcbc95253932917c4b54dc349be83b
eunhuit/정은후 <51870454+eunhuit@users.noreply.github.com>
2024-11-26 10:40:57 +0900

    Create SOFTinterrupt.c

---
+++ Servo/SOFTinterrupt.c
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+    DDRB=0xff;
+	TCCR1A = 0b10000010;
+	TCCR1B = 0b00011011; 
+	ICR1=4608;
+	OCR1A=346;
+	sei();
+    while (1) 
+    {
+		for(int i=145; i < 554; i++) {
+			OCR1A = i;
+			_delay_ms(10);
+    }
+}
+}
+
+
